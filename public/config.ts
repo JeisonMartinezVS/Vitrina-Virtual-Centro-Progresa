f27eb89b35db86a3b68f15db2663c1bac6311cee
JeisonMartinezVS <jeison.martinez@virtualsoft.tech>
2025-04-18 20:00:01 -0500

    Add configuration file and update components to utilize dynamic data for carousel, categories, and entrepreneurship sections

---
+++ public/config.ts
@@ -0,0 +1,139 @@
+export const config = {
+    "entrepreneurship": [
+        {
+            "id": 1,
+            "title": "Tecno Moda",
+            "description": "Esto es un ejemplo",
+            "entrepreneur": "Jeison",
+            "categories": "Moda",
+            "social_links": {
+                "facebook": "https://www.facebook.com/jeison",
+                "instagram": "https://www.instagram.com/jeison",
+                "twitter": "https://www.twitter.com/jeison",
+                "linkedin": "https://www.linkedin.com/in/jeison",
+            },
+            "image": "images/carousel1.png",
+        },
+        {
+            "id": 2,
+            "title": "Tecno Moda",
+            "description": "Esto es un ejemplo",
+            "entrepreneur": "Andres",
+            "categories": "Moda",
+            "social_links": {
+                "facebook": "https://www.facebook.com/jeison",
+                "instagram": "https://www.instagram.com/jeison",
+                "twitter": "https://www.twitter.com/jeison",
+                "linkedin": "https://www.linkedin.com/in/jeison",
+            },
+            "image": "images/carousel1.png",
+        },
+        {
+            "id": 3,
+            "title": "Tecno Moda",
+            "description": "Esto es un ejemplo",
+            "entrepreneur": "Camila",
+            "categories": "Moda",
+            "social_links": {
+                "facebook": "https://www.facebook.com/jeison",
+                "instagram": "https://www.instagram.com/jeison",
+                "twitter": "https://www.twitter.com/jeison",
+                "linkedin": "https://www.linkedin.com/in/jeison",
+            },
+            "image": "images/carousel1.png",
+        },
+        {
+            "id": 2,
+            "title": "Tecno Moda",
+            "description": "Esto es un ejemplo",
+            "entrepreneur": "Andres",
+            "categories": "Moda",
+            "social_links": {
+                "facebook": "https://www.facebook.com/jeison",
+                "instagram": "https://www.instagram.com/jeison",
+                "twitter": "https://www.twitter.com/jeison",
+                "linkedin": "https://www.linkedin.com/in/jeison",
+            },
+            "image": "images/carousel1.png",
+        },
+        {
+            "id": 3,
+            "title": "Tecno Moda",
+            "description": "Esto es un ejemplo",
+            "entrepreneur": "Camila",
+            "categories": "Moda",
+            "social_links": {
+                "facebook": "https://www.facebook.com/jeison",
+                "instagram": "https://www.instagram.com/jeison",
+                "twitter": "https://www.twitter.com/jeison",
+                "linkedin": "https://www.linkedin.com/in/jeison",
+            },
+            "image": "images/carousel1.png",
+        }
+    ],
+    "categories": [
+        {
+            "id": 1,
+            "name": "Moda",
+            "icon": "fa-solid fa-suitcase"
+        },
+        {
+            "id": 2,
+            "name": "Tecnología",
+        },
+        {
+            "id": 3,
+            "name": "Alimentación",
+        },
+        {
+            "id": 4,
+            "name": "Salud",
+        },
+        {
+            "id": 5,
+            "name": "Educación",
+        }
+    ],
+    "header": {
+        "menu": [
+            {
+                "id": 1,
+                "name": "Inicio",
+                "link": "/"
+            },
+            {
+                "id": 2,
+                "name": "Emprendimientos",
+                "link": "#entrepreneurship"
+            },
+            {
+                "id": 3,
+                "name": "Categorías",
+                "link": "/categories"
+            },
+            {
+                "id": 4,
+                "name": "Contacto",
+                "link": "/contact"
+            }
+        ],
+        "logo": "Logo_VVCP.png"
+    },
+    "footer": {},
+    "carousel": [
+        {
+            "id": 1,
+            "image": "images/PruebaImagen.png",
+            "title": "Emprendimiento 1",
+            "link": "https://www.facebook.com/",
+            "target": "_blank",
+        },
+        {
+            "id": 1,
+            "image": "images/PruebaImagen.png",
+            "title": "Emprendimiento 1",
+            "link": "https://www.facebook.com/",
+            "target": "_blank",
+        }
+    ]
+}
