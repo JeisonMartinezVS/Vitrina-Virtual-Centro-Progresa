f61529d64a63756b254b8f508bef92ca5e1f1693
JeisonMartinezVS <jeison.martinez@virtualsoft.tech>
2025-05-24 15:46:23 -0500

    Refactor entrepreneurship and categories components to utilize dynamic data, update modal functionality, and enhance carousel layout with new images.

--- public/config.ts
+++ public/config.ts
@@ -1,103 +1,4 @@
 export const config = {
-    "entrepreneurship": [
-        {
-            "id": 1,
-            "title": "Tecno Moda",
-            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci suscipit, saepe iure, amet perferendis qui quod fugiat animi delectus rem eaque, atque inventore praesentium doloremque magnam nostrum repellendus nulla assumenda.",
-            "entrepreneur": "Jeison",
-            "categories": "Moda",
-            "social_links": {
-                "facebook": "https://www.facebook.com/jeison",
-                "instagram": "https://www.instagram.com/jeison",
-                "twitter": "https://www.twitter.com/jeison",
-                "linkedin": "https://www.linkedin.com/in/jeison",
-            },
-            "image": "images/carousel1.png",
-        },
-        {
-            "id": 2,
-            "title": "Tecno Moda",
-            "description": "Esto es un ejemplo2",
-            "entrepreneur": "Andres",
-            "categories": "Moda",
-            "social_links": {
-                "facebook": "https://www.facebook.com/jeison",
-                "instagram": "https://www.instagram.com/jeison",
-                "twitter": "https://www.twitter.com/jeison",
-                "linkedin": "https://www.linkedin.com/in/jeison",
-            },
-            "image": "images/carousel1.png",
-        },
-        {
-            "id": 3,
-            "title": "Tecno Moda",
-            "description": "Esto es un ejemplo3",
-            "entrepreneur": "Camila",
-            "categories": "Moda",
-            "social_links": {
-                "facebook": "https://www.facebook.com/jeison",
-                "instagram": "https://www.instagram.com/jeison",
-                "twitter": "https://www.twitter.com/jeison",
-                "linkedin": "https://www.linkedin.com/in/jeison",
-            },
-            "image": "images/carousel1.png",
-        },
-        {
-            "id": 2,
-            "title": "Tecno Moda",
-            "description": "Esto es un ejemplo",
-            "entrepreneur": "Andres",
-            "categories": "Moda",
-            "social_links": {
-                "facebook": "https://www.facebook.com/jeison",
-                "instagram": "https://www.instagram.com/jeison",
-                "twitter": "https://www.twitter.com/jeison",
-                "linkedin": "https://www.linkedin.com/in/jeison",
-            },
-            "image": "images/carousel1.png",
-        },
-        {
-            "id": 3,
-            "title": "Tecno Moda",
-            "description": "Esto es un ejemplo",
-            "entrepreneur": "Camila",
-            "categories": "Moda",
-            "social_links": {
-                "facebook": "https://www.facebook.com/jeison",
-                "instagram": "https://www.instagram.com/jeison",
-                "twitter": "https://www.twitter.com/jeison",
-                "linkedin": "https://www.linkedin.com/in/jeison",
-            },
-            "image": "images/carousel1.png",
-        }
-    ],
-    "categories": [
-        {
-            "id": 1,
-            "name": "Moda",
-            "icon": "fa-solid fa-suitcase"
-        },
-        {
-            "id": 2,
-            "name": "Tecnología",
-        },
-        {
-            "id": 3,
-            "name": "Alimentación",
-        },
-        {
-            "id": 4,
-            "name": "Salud",
-        },
-        {
-            "id": 5,
-            "name": "Educación",
-        },
-        {
-            "id": 6,
-            "name": "Prueba",
-        }
-    ],
     "header": {
         "menu": [
             {
@@ -127,16 +28,16 @@ export const config = {
     "carousel": [
         {
             "id": 1,
-            "image": "images/PruebaImagen.png",
+            "image": "images/img.png",
             "title": "Emprendimiento 1",
-            "link": "https://www.facebook.com/",
-            "target": "_blank",
+            "link": "/",
+            "target": "",
         },
         {
             "id": 1,
-            "image": "images/PruebaImagen.png",
+            "image": "images/img2.jpeg",
             "title": "Emprendimiento 1",
-            "link": "https://www.facebook.com/",
+            "link": "https://forms.office.com/r/B6F4KMSb1U",
             "target": "_blank",
         }
     ]
